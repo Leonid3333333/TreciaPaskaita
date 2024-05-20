99d9a1d0922052405a65ff50ef7832823ec5e981
leonid rogaciov <l.rogaciov@gmail.com>
2024-05-20 10:48:34 +0300

    Add project files.

---
+++ TreciaPaskaita/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreciaPaskaita
+{
+   public class Book
+    {
+       public string title;
+       public string author;
+       public int pages;
+       public static int bookCount = 0;
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+            bookCount++;
+
+
+        }
+    }
+}
---
+++ TreciaPaskaita/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TreciaPaskaita
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Book book1 = new Book("Harry Porter", "JL Roeling", 400);
+
+            Book book2 = new Book("Lord of the Rings", "Tolkein", 700);
+            Console.WriteLine(Book.bookCount);
+
+            Console.WriteLine(book1.title);
+
+        }
+    }
+
+
+}
